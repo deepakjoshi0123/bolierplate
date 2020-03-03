d89bbc20de94ca5559f077014039bf22a44e3309
Deepak joshi <deepakjoshi0123@gmail.com>
2020-03-02 18:46:18 -0800

    approx

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,7 @@
 const product = require('../model/products');
-
 //adding products 
 class productController {
-    static postAddProducts = (req, res, next) => {
+    static postAddProducts = async(req, res, next) => {
         logger.info('going in postaddproducts');
         const productName = req.body.ProductName;
         const productType = req.body.productType;
@@ -17,34 +16,39 @@ class productController {
             error.data = errors.array();
             throw error;
         }
-
-        product.create({
+     try{
+       const result = await product.create({
             ProductName: productName,
             ProductType: productType,
             price: price
-        }).then(result => {
-            console.log('product added');
+        }) 
+
+            console.log('product added')
             res.json({ message: "product added succesfully" });
-        }).catch(err => {
+           }
+        catch(err ) {
             console.log(err);
-        });
+        }
     }
     //fetching all the products 
-    static getProducts = (req, res, next) => {
+    static getProducts = async(req, res, next) => {
         const start = req.params.start;
-        console.log(start);
         const pageSize = 3;
-        product.findAll({
+       try{
+        const products =await product.findAll({
             offset: 0,
             limit: 10
         })
-            .then(products => {
                 res.json({ product: products })
-            });
     }
-
+      catch(error)
+      {
+        console.log(error);
+        next(error);
+      }
+    }
     //updating products
-    static posteditproduct = (req, res, next) => {
+    static posteditproduct = async(req, res, next) => {
         const ProductName = req.body.ProductName;
         const ProductType = req.body.ProductType;
         const price = req.body.price;
@@ -57,39 +61,46 @@ class productController {
             error.data = errors.array();
             throw error;
         }
-
-        product.findByPk(id)
-            .then(product => {
+       try{
+        const product = await product.findByPk(id);
                 product.ProductName = ProductName;
                 product.ProductType = ProductType;
                 product.price = price;
-                return product.save();
-            })
-            .then(resutl => {
-                console.log('product updated '); // send json res for success
-            })
+                 product.save();
+               console.log('product updated ')
+        }
+        catch(erro)
+        {
+            console.log(error);
+            next(error);
+        }   
     }
-    static postDelProduct = (req, res, next) => {
+    static postDelProduct = async(req, res, next) => {
         const id = req.params.id;
-        product.destroy({
+        try{
+        const result =await  product.destroy({
             where: { id: id }
-        }).then(result => {
+        })
             console.log("product deleted");
             res.json({ msg: "prodcut deleted" })
-        }).catch(err => {
-            console.log(err);
-        })
+          }
+       catch(err ) {
+        console.log(error);
+        next(error);
+        }
     }
-    static searchProductdetails = (req, res, next) => {
+    static searchProductdetails = async(req, res, next) => {
         const ProductName = req.params.ProductName;
-        console.log(ProductName);
-        product.findAll({ where: { ProductName: ProductName } })
+         try{
+        const result = await product.findAll({ where: { ProductName: ProductName } })
             .then(result => {
                 res.json({ user: result })
             })
-            .catch(err => {
-                console.log(err);
-            })
+          }
+            catch(err) {
+                console.log(error);
+                next(error);
+            }
     }
 }
 module.exports = productController;
